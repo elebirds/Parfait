bd9b7e60d9867a8d160fe5afc03e5ebd34535d0e
MidRatKing <3214443636@qq.com>
2025-03-31 18:36:08 +0800

    [测试] UUID输出测试

--- src/test/kotlin/POITest.kt
+++ src/test/kotlin/POITest.kt
@@ -1,17 +1,19 @@
 import com.deepoove.poi.XWPFTemplate
 import com.deepoove.poi.data.Documents
 import java.io.FileOutputStream
+import java.util.UUID
 
 fun main() {
-    val template: XWPFTemplate = XWPFTemplate.compile("C:\\Users\\hhmcn\\Desktop\\model.docx").render(hashMapOf<String, Any>(
-        "snameCHI" to "郑植",
-        "snameENG" to "Zheng Zhi",
-        "sgenderCHI" to "男",
-        "sid" to 220153061,
-        "sgrade" to 2022,
-        "sprofCHI" to "计算机科学与技术（中法合作）",
-        "sgradeweighted" to 4.0,
-        "date" to "2023年10月01日",
-    ))
-    template.writeAndClose(FileOutputStream("C:\\Users\\hhmcn\\Desktop\\output.docx"))
+    print(UUID.randomUUID().toString())
+    //val template: XWPFTemplate = XWPFTemplate.compile("C:\\Users\\hhmcn\\Desktop\\model.docx").render(hashMapOf<String, Any>(
+    //    "snameCHI" to "郑植",
+    //    "snameENG" to "Zheng Zhi",
+    //    "sgenderCHI" to "男",
+    //    "sid" to 220153061,
+    //    "sgrade" to 2022,
+    //    "sprofCHI" to "计算机科学与技术（中法合作）",
+    //    "sgradeweighted" to 4.0,
+    //    "date" to "2023年10月01日",
+    //))
+    //template.writeAndClose(FileOutputStream("C:\\Users\\hhmcn\\Desktop\\output.docx"))
 }
