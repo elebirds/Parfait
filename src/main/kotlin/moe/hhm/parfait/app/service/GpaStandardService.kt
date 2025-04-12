6121bed26ac2c9546b834aca0b1bad4da066a460
elebirds <hhmcn@outlook.com>
2025-04-12 18:54:11 +0800

    [服务] GPA服务简单实现

--- src/main/kotlin/moe/hhm/parfait/app/service/GpaStandardService.kt
+++ src/main/kotlin/moe/hhm/parfait/app/service/GpaStandardService.kt
@@ -6,5 +6,16 @@
 
 package moe.hhm.parfait.app.service
 
+import moe.hhm.parfait.domain.model.gpa.GpaStandard
+import moe.hhm.parfait.dto.GpaStandardDTO
+import org.jetbrains.exposed.dao.id.EntityID
+import java.util.UUID
+
 interface GpaStandardService {
+    suspend fun getAllGpaStandards(): List<GpaStandardDTO>
+    suspend fun count(): Long
+    suspend fun getGpaStandardByName(name: String): GpaStandard?
+    suspend fun addGpaStandard(gpaStandard: GpaStandardDTO): EntityID<UUID>
+    suspend fun deleteGpaStandard(uuid: UUID): Boolean
+    suspend fun updateGpaStandard(gpaStandard: GpaStandardDTO): Boolean
 }
--- src/main/kotlin/moe/hhm/parfait/app/service/impl/GpaStandardServiceImpl.kt
+++ src/main/kotlin/moe/hhm/parfait/app/service/impl/GpaStandardServiceImpl.kt
@@ -7,7 +7,34 @@
 package moe.hhm.parfait.app.service.impl
 
 import moe.hhm.parfait.app.service.GpaStandardService
+import moe.hhm.parfait.domain.model.gpa.GpaStandard
 import moe.hhm.parfait.domain.repository.GpaRepository
+import moe.hhm.parfait.dto.GpaStandardDTO
+import org.jetbrains.exposed.dao.id.EntityID
+import java.util.UUID
 
 class GpaStandardServiceImpl(private val rep: GpaRepository) : GpaStandardService  {
+    override suspend fun getAllGpaStandards(): List<GpaStandardDTO> {
+        return rep.findAll().map { it.toDTO() }
+    }
+
+    override suspend fun count(): Long {
+        return rep.count()
+    }
+
+    override suspend fun getGpaStandardByName(name: String): GpaStandard? {
+        return rep.findByName(name)
+    }
+
+    override suspend fun addGpaStandard(gpaStandard: GpaStandardDTO): EntityID<UUID> {
+        return rep.add(gpaStandard)
+    }
+
+    override suspend fun deleteGpaStandard(uuid: UUID): Boolean {
+        return rep.delete(uuid)
+    }
+
+    override suspend fun updateGpaStandard(gpaStandard: GpaStandardDTO): Boolean {
+        return rep.update(gpaStandard)
+    }
 }
